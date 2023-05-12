4ecbd0c1680d4972e82b900fcbd47e19b7261771
pedrohenriquek <pedrohenriquesgtau@gmail.com>
2023-05-12 14:11:39 -0300

    enviando o código

---
+++ tesourp.py
@@ -0,0 +1,7 @@
+moedas = int(input())
+marinheiros = int(input())
+
+novosMarinheiros = marinheiros+2  
+marinheiro = moedas / novosMarinheiros
+capitao = marinheiro * 2 
+print (capitao)
---
+++ tuturial.py
@@ -0,0 +1,33 @@
+#mostrar na tela 
+print("mostrando algo na tela")
+
+#pegar uma informação
+input()
+
+#salvar uma informação na variavel
+variavel = input()
+
+#transformar a informação em numero
+numero = int(input())
+#criar uma lista
+lista = []
+
+#adicionar uma variavel na lista
+variavelnova = "batata"
+lista.append(variavelnova)
+
+#checar o item da lista 
+item = lista[0]
+
+#organisar uma lista 
+lista = [44,13,72]
+lista.sort()
+#lista = [44,13,72]
+
+
+#escolher quantos itens tem na lista 
+quantidade = int(input)
+listaFinal = []
+for rep in range(quantidade):
+    listaFinal.append(int(input()))
+    
