7efc82219462bb19b4f0826a3644a4c4761f75a3
pedrohenriquek <pedrohenriquesgtau@gmail.com>
2023-05-12 15:24:25 -0300

    morango

---
+++ bondi.py
@@ -0,0 +1,8 @@
+professores = int(input())
+alunos = int(input())
+bonde = professores + alunos
+if bonde >= 50:
+    print ("sim")
+else: 
+    print ("não")
+    
---
+++ morango.py
@@ -0,0 +1,10 @@
+parte1 = int(input())
+parte2 = int(input())
+parte3 = int(input())
+parte4 = int(input())
+area1 = parte1 * parte2
+area2 = parte3 * parte4
+if area1 > area2:
+    print (area1)
+else:
+    print (area2)   
